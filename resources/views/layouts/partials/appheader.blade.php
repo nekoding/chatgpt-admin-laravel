b73f8b6f42eb33f814f6c353743e96237615e5e2
enggar tivandi <enggar.appkey@gmail.com>
2023-04-06 10:25:35 +0800

    chore: remote appheader breadcrumb

--- resources/views/layouts/partials/appheader.blade.php
+++ resources/views/layouts/partials/appheader.blade.php
@@ -59,84 +59,22 @@
                 <div class="dropdown-menu dropdown-menu-end pt-0">
                     <div class="dropdown-header bg-light py-2">
                         <div class="fw-semibold">Account</div>
-                    </div><a
+                    </div>
+                    <a
                         class="dropdown-item"
                         href="#"
                     >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-                        </svg> Updates<span class="badge badge-sm bg-info ms-2">42</span></a><a
+                        <i class="icon me-2 cil-user"></i>Profil</a>
+                    <div class="dropdown-divider"></div>
+                    <a
                         class="dropdown-item"
                         href="#"
+                        x-data=""
+                        @click="axios.post('{{ route('logout') }}').then(res => window.location.reload())"
                     >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-                        </svg> Messages<span class="badge badge-sm bg-success ms-2">42</span></a><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-task"></use>
-                        </svg> Tasks<span class="badge badge-sm bg-danger ms-2">42</span></a><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-comment-square"></use>
-                        </svg> Comments<span class="badge badge-sm bg-warning ms-2">42</span></a>
-                    <div class="dropdown-header bg-light py-2">
-                        <div class="fw-semibold">Settings</div>
-                    </div><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
-                        </svg> Profile</a><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
-                        </svg> Settings</a><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
-                        </svg> Payments<span class="badge badge-sm bg-secondary ms-2">42</span></a><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
-                        </svg> Projects<span class="badge badge-sm bg-primary ms-2">42</span></a>
-                    <div class="dropdown-divider"></div><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-                        </svg> Lock Account</a><a
-                        class="dropdown-item"
-                        href="#"
-                    >
-                        <svg class="icon me-2">
-                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
-                        </svg> Logout</a>
+                        <i class="icon me-2 cil-account-logout"></i> Logout</a>
                 </div>
             </li>
         </ul>
-    </div>
-    <div class="header-divider"></div>
-    <div class="container-fluid">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb my-0 ms-2">
-                <li class="breadcrumb-item">
-                    <!-- if breadcrumb is single--><span>Home</span>
-                </li>
-                <li class="breadcrumb-item active"><span>Dashboard</span></li>
-            </ol>
-        </nav>
     </div>
 </header>
