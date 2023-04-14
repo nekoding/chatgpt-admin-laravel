edf40d36de0b9e97335e3c6ba7088442a9b94b7f
enggar tivandi <enggar.appkey@gmail.com>
2023-04-14 15:16:12 +0800

    chore: update schema force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Yajra\DataTables\Html\Builder;
 
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Builder::useVite();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
